66db320f50cf83694ad4e5676e08d22fe0910069
Adeel Fiaz <122649696+adeelfiaz1951@users.noreply.github.com>
2025-11-15 00:35:32 +0100

    user registaration and login validation using hash

--- auth.py
+++ auth.py
@@ -21,3 +21,47 @@ class AuthDB:
 
         conn.commit()
         conn.close()
+
+    # helper function hash password simple and safe
+
+    def hash_password(self, password):
+        """"Hashing password using SHA256"""
+        return hashlib.sha3_256(password.encode()).hexdigest()
+    
+    # Register new user
+
+    def register_user(self, username, email,password):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        hashed = self.hash_password(password)
+
+        try:
+            cursor.execute(
+                "INSERT INTO user (username, email, password) VALUES(?, ?, ?)",
+                (username, email, hashed)
+            )
+            conn.commit()
+            conn.close()
+            return True #'Registaration successfull'
+        except sqlite3.IntegrityError:
+            conn.close()
+            return False # user already exists
+        
+    # validate login
+
+    def validate_login(self, username, password):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        hashed = self.hash_password(password)
+        cursor.execute(
+            "SELECT * FROM user WHERE username = ? AND password = ?",
+            (username, password)
+            )
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            return True # Login successful
+        return False # Incorrect username            
